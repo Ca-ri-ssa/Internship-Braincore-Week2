89d8b79954b485279c24a5326dceafcecafe3921
carissa-chandra <carissa.tjan03@gmail.com>
2024-08-20 17:52:09 +0700

    HTTP server

--- Bagian6/index.ts
+++ Bagian6/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello via Bun!");
+const formHtml = await Bun.file("./form.html").text();
+
+const server = Bun.serve({
+    port: 3000,
+    async fetch(request, server) {
+        const url = new URL(request.url);
+        const method = request.method;
+
+        if (url.pathname === '/') {
+            return new Response(
+                formHtml, {
+                    headers: {
+                        "content-type": "text/html"
+                    }
+                }
+            );
+        } else if(url.pathname === '/data' && method === 'POST') {
+            const data = await request.text().then((body) => {
+                const param = new URLSearchParams(body);
+                return param;
+            })
+            console.log(data);
+
+            return new Response("POST request made");
+        } else {
+            return new Response("Hello World!");
+        }
+    },
+})
+
+console.log(`http://${server.hostname}:${server.port}`);
